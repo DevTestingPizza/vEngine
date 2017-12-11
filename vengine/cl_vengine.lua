50dc98ebf8962deb6fbb868bcb8c9414d9218dd9
Tom Grobbe <31419184+TomGrobbe@users.noreply.github.com>
2017-12-11 21:58:53 +0100

    Fixed bugs, rewrote code, added features
    ### Fixed
    - Compatibility issues with Luxarts Emergency Vehicles script.
    - Car turning back on right after turning the engine off.
    
    ### Added
    - (optional, disabled by default) `/engine` command to toggle the engine.
    - Easy configuration option to change the button (F6/167 by default)
    
    ### Reworked
    Reworked code to be clearner, use newest manifest version and new natives. And just overall improved the quality.

--- vengine/cl_vengine.lua
+++ vengine/cl_vengine.lua
@@ -1,12 +1,34 @@
+-- Configuration
+
+local button = 167 -- 167 (F6 by default)
+local commandEnabled = false -- (false by default) If you set this to true, typing "/engine" in chat will also toggle your engine.
+
+-- You're all set now!
+
+
+-- Code, no need to modify this, unless you know what you're doing or you want to fuck shit up.
+-- No support will be provided if you modify this part below.
+
 Citizen.CreateThread(function()
+    if commandEnabled then
+        RegisterCommand('engine', function() 
+            toggleEngine()
+        end, false)
+    end
     while true do
         Citizen.Wait(0)
-        if IsControlJustReleased(0, 167) then
-            if GetVehiclePedIsIn(GetPlayerPed(-1), false) ~= nil then
-                if GetPedInVehicleSeat(GetVehiclePedIsIn(GetPlayerPed(-1), false)) < 1 then
-                   SetVehicleEngineOn(GetVehiclePedIsIn(GetPlayerPed(-1), false), (not IsVehicleEngineOn(GetVehiclePedIsIn(GetPlayerPed(-1), false))), true, false)
-                end
-            end
+        local vehicle = GetVehiclePedIsIn(PlayerPedId(), false)
+        
+        if (IsControlJustReleased(0, button) or IsDisabledControlJustReleased(0, button)) and vehicle ~= nil and vehicle ~= 0 and GetPedInVehicleSeat(vehicle, 0) then
+            toggleEngine()
         end
+        
     end
 end)
+
+function toggleEngine()
+    local vehicle = GetVehiclePedIsIn(PlayerPedId(), false)
+    if vehicle ~= nil and vehicle ~= 0 and GetPedInVehicleSeat(vehicle, 0) then
+        SetVehicleEngineOn(vehicle, (not GetIsVehicleEngineRunning(vehicle)), false, true)
+    end
+end
